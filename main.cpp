91186350ba72c72612461593f13c6347076ede8c
teaf1001 <teaf1001@naver.com>
2021-07-15 20:14:02 -0400

    edit main.cpp(3)

--- main.cpp
+++ main.cpp
@@ -3,14 +3,6 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 
-uint32_t my_ntohl(uint32_t a) {
-    uint32_t a1 = (a & 0xFF000000) >> 3 * 8;
-    uint32_t a2 = (a & 0x00FF0000) >> 8;
-    uint32_t a3 = (a & 0x0000FF00) << 8;
-    uint32_t a4 = (a & 0x000000FF) << 3 * 8;
-    return a1 | a2 | a3 | a4;
-}
-
 int main(int argc, char* argv[])
 {
     //FILE *Pfh = fopen("./five-hundred.bin", "rb");//500
